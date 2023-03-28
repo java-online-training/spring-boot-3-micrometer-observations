48f9710e109f7f9cf63f9f9524601ac4b36a9c59
superernie77 <superernie77@gmail.com>
2023-03-28 19:23:31 +0200

    initial commit

---
+++ src/main/java/com/jot/springboot3micrometerobservations/SpringBoot3MicrometerObservationsApplication.java
@@ -0,0 +1,13 @@
+package com.jot.springboot3micrometerobservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3MicrometerObservationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3MicrometerObservationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jot/springboot3micrometerobservations/SpringBoot3MicrometerObservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jot.springboot3micrometerobservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3MicrometerObservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
