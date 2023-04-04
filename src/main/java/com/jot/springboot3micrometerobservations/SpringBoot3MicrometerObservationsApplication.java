0a716a61ae4da0936d3603e364886cc295e2e3ad
superernie77 <superernie77@gmail.com>
2023-04-04 10:00:48 +0200

    enabled prometheus  metrics

--- src/main/java/com/jot/springboot3micrometerobservations/SpringBoot3MicrometerObservationsApplication.java
+++ src/main/java/com/jot/springboot3micrometerobservations/SpringBoot3MicrometerObservationsApplication.java
@@ -1,6 +1,5 @@
 package com.jot.springboot3micrometerobservations;
 
-import io.micrometer.observation.annotation.Observed;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,5 +9,4 @@ public class SpringBoot3MicrometerObservationsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot3MicrometerObservationsApplication.class, args);
 	}
-
 }
