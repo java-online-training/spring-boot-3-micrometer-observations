b4312b9bdd0568bf8dd4569b477467ef0fc50cba
superernie77 <superernie77@gmail.com>
2023-04-06 09:32:48 +0200

    added logging + zipkin properties

--- src/main/java/com/jot/springboot3micrometerobservations/controller/GreetingController.java
+++ src/main/java/com/jot/springboot3micrometerobservations/controller/GreetingController.java
@@ -1,13 +1,18 @@
 package com.jot.springboot3micrometerobservations.controller;
 
+import org.slf4j.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class GreetingController {
 
+    private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+
     @GetMapping("/greeting")
     public Greeting getGreeting(){
+        log.info("Request received.");
         return new Greeting("Hello!");
     }
 
